d86400d5754b2dad99b0cf885443cd65d204f8d0
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-03-14 09:37:51 +0100

    Finished the API application for the CLI (exercise 6)

--- apis/Exercise_06.py
+++ apis/Exercise_06.py
@@ -24,7 +24,7 @@ tasks_url = "http://demo.codingnomads.co:8080/tasks_api/tasks"
 
 
 def main():
-    '''A function to setup the menu for the user and print the options to the screen'''
+    """A function for the menu and the program logic"""
 
     print(
         "----------------------------------------\n"
@@ -55,32 +55,34 @@ def main():
     elif user_input == 4:
         view_incomplete_tasks_4()
     elif user_input == 5:
-        pass
+        create_new_task_5()
     elif user_input == 6:
-        pass
+        update_task_6()
     elif user_input == 7:
-        pass
+        delete_task_7()
     else:
         print("Whooops! Something went terribly wrong!")
     return
 
 
+# done
 def verify_input(user_input):  # what do I do with strings??
-    '''A function to verify that the user entered a correct number of the menu'''
+    """A function to verify that the user entered is a correct number of the menu"""
 
     try:
         if 0 < user_input < 8:
             "----------------------------------------\n"
             print("Thank you for your input!")
         else:
-            print("There has been a mistake. Please enter a number from the menu: ")
+            print("There has been a mistake. Please enter a number from the menu. ")
             main()
     except ValueError:
         print("Please enter a number from the menu.")
 
 
+# done
 def verify_byebye(user_input):
-    '''A function to verify the input to proceed or exit the app'''
+    """A function to verify the input to proceed or exit the app"""
 
     try:
         if user_input.lower() == 'm':
@@ -92,8 +94,25 @@ def verify_byebye(user_input):
         question_afterwards()
 
 
+'''
+def check_completion_task(user_comp):
+    """A function to convert user input whether a task is completed or not"""
+
+    try:
+        if user_comp.lower() == 'y':
+            user_comp = True
+        elif user_comp.lower() == 'n':
+            user_comp = False
+    except ValueError:
+        print("Your input was not correct.")
+    
+    return user_comp
+'''
+
+
+# done
 def create_account_1():
-    '''A function for the first point of the menu'''
+    """A function to create a new user"""
 
     print("----------------------------------------\n")
     print("You want to create a new account. Therefore we need your first name, last name \n"
@@ -101,7 +120,7 @@ def create_account_1():
     print("----------------------------------------\n")
     user_first = str(input("Please enter your first name: --> "))
     user_last = str(input("Please enter your last name: --> "))
-    user_mail = str(input("Please enter your email: --> "))
+    user_mail = str(input("Finally please enter your email: --> "))
 
     body = {
         'first_name': f"{user_first}",
@@ -117,8 +136,9 @@ def create_account_1():
     question_afterwards()
 
 
+# done
 def view_all_tasks_2():
-    '''A function to view all tasks'''
+    """A function to view all tasks"""
 
     print("----------------------------------------\n")
     print("You want to see all your tasks. Therefore we need your UserId.")
@@ -135,8 +155,9 @@ def view_all_tasks_2():
     question_afterwards()
 
 
+# done
 def view_completed_tasks_3():
-    '''A function to view the completed tasks'''
+    """A function to view the completed tasks"""
 
     print("----------------------------------------\n")
     print("You want to see your completed tasks. Therefore we need your UserId.")
@@ -153,11 +174,12 @@ def view_completed_tasks_3():
     question_afterwards()
 
 
+# done
 def view_incomplete_tasks_4():
-    '''A function to view the incompleted tasks'''
+    """A function to view the incomplete tasks"""
 
     print("----------------------------------------\n")
-    print("You want to see your completed tasks. Therefore we need your UserId.")
+    print("You want to see your incomplete tasks. Therefore we need your UserId.")
     print("----------------------------------------\n")
     userId_input = input("Please enter your UserId: --> ")
 
@@ -171,16 +193,77 @@ def view_incomplete_tasks_4():
     question_afterwards()
 
 
+# done
 def create_new_task_5():
-    pass
+    """A function to create a new task"""
+
+    print("----------------------------------------\n")
+    print("You want to create a new task. Therefore we need the name of the task, a description \n"
+          "and your UserId to connect the task to your account.")
+    print("----------------------------------------\n")
+    user_name = str(input("Please enter the name of the task: --> "))
+    user_desc = str(input("Please enter a description of the task: --> "))
+    user_id = str(input("Please enter your UserId: --> "))
+
+    body = {
+        'name': f"{user_name}",
+        'description': f"{user_desc}",
+        'userId': f"{user_id}"
+    }
+
+    response = requests.post(tasks_url, json=body)
+
+    print(f"Response status code: {response.status_code} \n"
+          f"A new task for user {user_id} has successfully been created!")
+
+    question_afterwards()
 
 
+# done
 def update_task_6():
-    pass
+    """A function to update an existing task"""
+
+    print("----------------------------------------\n")
+    print("You want to update a task. Therefore we need the name of the task, a description \n"
+          "and a UserId to connect the task to an account.")
+    print("----------------------------------------\n")
+    user_taskid = str(input("Please enter the Id of the task you want to update: --> "))
+    user_name = str(input("Please enter the new name of the task: --> "))
+    user_desc = str(input("Please enter a new description of the task: --> "))
+    user_id = str(input("Please enter the UserId: --> "))
+    user_comp = str(input("Finally: Is the task completed yet? [true/false] --> "))
+
+    body = {
+        'id': f"{user_taskid}",
+        'name': f"{user_name}",
+        'description': f"{user_desc}",
+        'userId': f"{user_id}",
+        'completed': f"{user_comp}"
+    }
+
+    response = requests.put(tasks_url, json=body)
+
+    print(f"Response code: {response.status_code} \n"
+          f"The task with Id {user_taskid} has successfully been updated!")
+
+    question_afterwards()
 
 
+# done
 def delete_task_7():
-    pass
+    """A function to delete a task"""
+
+    print("----------------------------------------\n")
+    print("You want to delete a task. Therefore we need the Id of the task.")
+    print("----------------------------------------\n")
+    user_taskid = str(input("Please enter the Id of the task you want to delete: --> "))
+
+    response = requests.delete(tasks_url + f"/{user_taskid}")
+
+    print(f"Response status code: {response.status_code} \n"
+          f"The task with Id {user_taskid} has successfully been deleted!")
+
+    question_afterwards()
 
 
 def question_afterwards():
