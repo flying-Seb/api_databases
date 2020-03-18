763040ad1bb3fd1fc10ab1c0aff2666bd254f6a9
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-03-18 21:45:59 +0100

    worked on join statements

--- databases/Exercise_02.py
+++ databases/Exercise_02.py
@@ -5,12 +5,13 @@ Consider each of the tasks below as a separate database query. Using SQLAlchemy,
 1) Select all the actors with the first name of your choice
 2) Select all the actors and the films they have been in
 3) Select all the actors that have appeared in a category of your choice
-4) Select all the comedic films and sort them by rental rate
+4) Select all the comedy films and sort them by rental rate
 5) Using one of the statements above, add a GROUP BY statement of your choice
 6) Using one of the statements above, add a ORDER BY statement of your choice
 
 '''
 
+from sqlalchemy import desc
 import sqlalchemy
 import pymysql
 from pprint import pprint
@@ -52,8 +53,37 @@ pprint(result_set)
 
 ''' 3) select all actors that have appeared in the comedy category '''
 
-# use another join statement
+# the actor and the film_actor object can be used from above for this query
+category = sqlalchemy.Table('category', metadata, autoload=True, autoload_with=engine)
+film_cat = sqlalchemy.Table('film_category', metadata, autoload=True, autoload_with=engine)
+
+# create new join statement for this query
+join_statement = actor.join(film_actor, film_actor.columns.actor_id == actor.columns.actor_id).\
+                 join(film_cat, film_cat.columns.film_id == film_actor.columns.film_id).join(\
+                 category, category.columns.category_id == film_cat.columns.category_id)
+
+# select statement of the join statement
+query = sqlalchemy.select([category.columns.name, actor.columns.first_name,\
+                           actor.columns.last_name]).where(category.columns.name == 'Comedy')\
+                           .select_from(join_statement)
+
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchmany(25)
+print('Exercise 3)')
+pprint(result_set)
 
 
+''' 4) select all comedy films and sort by rental rate '''
 
+# use category, film and film_cat from previous exercises
+# create join from category over film_cat to film to access rental_rate
+join_statement = category.join(film_cat, film_cat.columns.category_id == category.columns.category_id).\
+                 join(film, film.columns.film_id == film_cat.columns.film_id)
 
+query = sqlalchemy.select([category.columns.name, film.columns.title, film.columns.rental_rate]).where(category.columns.\
+        name == 'Comedy').order_by(desc(film.columns.rental_rate)).select_from(join_statement)
+
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+print('Exercise 4)')
+pprint(result_set)
