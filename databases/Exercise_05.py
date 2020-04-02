13de447a345b8f3dd8b4ebeac3aecaaa2c52eb78
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-04-02 21:15:12 +0200

    fine tuning on the database and get requests to the API implemented

--- databases/Exercise_05.py
+++ databases/Exercise_05.py
@@ -15,12 +15,42 @@ To prevent this, you should add a check to see if the record already exists befo
 '''
 
 # first: paper and pen for architecture! --> done
-# database created over the CLI
+# database created over the CLI --> done
+# requests.get all users and all tasks from API --> done
 
 import requests
 from pprint import pprint
+import json
 
+users_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+tasks_url = "http://demo.codingnomads.co:8080/tasks_api/tasks"
+
+
+def get_users():
+    """A function to get all users and save it in a JSON format"""
+
+    response = requests.get(users_url)
+    all_users = response.json()
+    return all_users
+
+
+def get_tasks():
+    """A function to get all tasks and save it in a JSON format"""
+
+    response = requests.get(tasks_url)
+    all_tasks = response.json()
+    return all_tasks
+
+
+all_users = get_users()
+all_tasks = get_tasks()
+
+pprint(all_users)
+print("/n")
+print("---------------------------------------------")
+print("/n")
+pprint(all_tasks)
 
 # setting name to main if the program is called directly
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#    main()
